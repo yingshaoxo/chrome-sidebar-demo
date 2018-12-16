9efc598a74bc870f8f8d5e36519507624589b90f
yingshaoxo <yingshaoxo@gmail.com>
2018-12-16 12:44:16 +0800

    we can have more attempts

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,24 @@ import Navigation from './components/Navigation';
 class App extends React.Component {
     render() {
         return (
-            <Navigation></Navigation>
+            <iframe
+                style={{
+                    zIndex: 999999, 
+                    position: "fixed", 
+
+                    display: 'flex',  
+                    justifyContent:'left', 
+                    alignItems:'left', 
+
+                    height: '100vh',
+                    width: '10vw',
+
+                    backgroundColor: "#C0C0C0",
+                    overflowX: "hidden",
+                }}
+            >
+                <Navigation></Navigation>
+            </iframe>
         )
     }
 }
@@ -42,14 +59,13 @@ function injectApp() {
     html_element.insertAdjacentHTML('afterbegin', newDiv.innerHTML)
     */
 
-    /*
-    var id = "chrome_sidebar_demo_dom"
+    var id = "chrome_sidebar_demo"
     const newDiv = document.createElement("div");
     newDiv.setAttribute("id", id);
     document.body.appendChild(newDiv);
     ReactDOM.render(<App />, newDiv);
-    */
 
+    /*
     var id = "chrome_sidebar_demo_dom"
     const newDiv = document.createElement("div");
     newDiv.setAttribute("id", id);
@@ -57,10 +73,9 @@ function injectApp() {
     var html_element = document.getElementsByTagName("html")[0]
     html_element.insertAdjacentHTML('afterbegin', newDiv.innerHTML)
 
-    console.log(document.getElementById(id))
     document.body.appendChild(newDiv)
     ReactDOM.render(<App />, newDiv);
-    console.log(document.getElementById(id))
+    */
 }
 
 injectApp()
